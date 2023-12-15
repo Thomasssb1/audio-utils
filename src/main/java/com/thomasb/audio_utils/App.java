0071f7c43abc58faf369a1863fe296c429b702fd
Thomas Beer <thomas.beer04@outlook.com>
2023-12-15 18:57:02 +0000

    Initial commit

---
+++ src/main/java/com/thomasb/audio_utils/App.java
@@ -0,0 +1,13 @@
+package com.thomasb.audio_utils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
